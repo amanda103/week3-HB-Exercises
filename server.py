eb93cbc5f9c36018143a95c1352d229a07a99f29
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-19 13:10:38 -0700

    added compliment feature

--- server.py
+++ server.py
@@ -18,7 +18,13 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+    <!doctype html>
+    <html>
+    Hi! This is the home page.
+    <a href="http://localhost:5000/hello">Go to Hello!</a>
+    </html>
+    """
 
 
 @app.route('/hello')
@@ -35,11 +41,66 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+          <select name="compliment">
+            <option value="awesome">Awesome</option>
+            <option value="terrific">Terrific</option>
+            <option value="fantastic">Fantastic</option>
+            <option value="neato">Neato</option>
+            <option value="fabulous">Fabulous</option>
+            <option value="wowza">Wowza</option>
+            <option value="oh-so-not-meh">Oh-so-not-meh</option>
+            <option value="brilliant">Brilliant</option>
+            <option value="ducky">Ducky</option>
+            <option value="coolio">Coolio</option>
+            <option value="incredible">Incredible</option>
+            <option value="wonderful">Wonderful</option>
+            <option value="smashing">Smashing</option>
+            <option value="lovely">Lovley</option>
+          </select>
           <input type="submit" value="Submit">
+        </form>
+        <br>
+        <form action="/diss">
+          What's your name? <input type="text" name="person">
+          <select name="diss">
+            <option value="poop head">poop head</option>
+            <option value="butt head">butt head</option>
+            <option value="disgrace">disgrace</option>
+            <option value="DUMMY!">DUMMY!</option>
+            <option value="little fart! (just a little one)">little fart</option>
+            <option value="crocodile (they are awful!)">crocodile</option>
+            <option value="nincompoop!">nincompoop!</option>
+          </select>
+          <input type="submit" value="Submit">
+          <br>
+          <br>
+          Are they ugly? <input type="radio" name="isugly" value="yes">Yes
+                          <input type="radio" name="isugly" value="no">No
         </form>
       </body>
     </html>
     """
+
+
+@app.route('/diss')
+def diss_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    diss = request.args.get("diss")
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {}! I think you are a {}!
+      </body>
+    </html>
+    """.format(player, diss)
 
 
 @app.route('/greet')
@@ -48,7 +109,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
 
     return """
     <!doctype html>
